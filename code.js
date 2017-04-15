1c9dd4183a1ac49093eb0d065e19bff00da22269
Aditya Thebe <Aditya Thebe>
2017-04-15 10:05:54 +0545

    adding setinterval function

--- code.js
+++ code.js
@@ -1,19 +1,24 @@
-$.getJSON('http://api.open-notify.org/astros.json', gotdata);
+reload();
 
-function gotdata(data)	{
-	var n = data.people.length;
-	var heading = "People in Outer Space right now : " + "<span id=\"bold\">" + n + "</strong>" ;
-	$('#header').html(heading);
+function reload()   {
+    $("ol").empty()
+    $.getJSON('http://api.open-notify.org/astros.json', gotdata);
+}
 
-	for(var i = 0; i < n ; i++)	{
-    	var person = data.people[i].name;
-    	var craft = data.people[i].craft;
-    	var output = "<li>" + person + " - " + craft + "</li>";
-    	
+function gotdata(data)  {
+    var n = data.people.length;
+    var heading = "Humans in Outer Space right now : " + "<span id=\"bold\">" + n + "</strong>" ;
+    $('#header').html(heading);
+
+    for(var i = 0; i < n ; i++) {
+        var person = data.people[i].name;
+        var craft = data.people[i].craft;
+        var output = "<li>" + person + " - " + craft + "</li>";
+        
         $("ol").append(output);
     }
-    var source = "Source : <a href=\"http://open-notify.org/\" target=\"_blank\">Open Notify</a>";
+    var source = "Code : <a href=\"https://github.com/adityathebe/Space.git/\" target=\"_blank\">Github</a>";
     $('#footer').html(source);
 }
 
- 
+setInterval(reload, 1000 * 60 * 10 );
